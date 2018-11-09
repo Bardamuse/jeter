dd73e25544956855402fd6459a3fb9defb082ee6
Salamano <philippebesnier@free.fr>
2018-11-09 09:20:30 +0100

    ajout de datab.py

---
+++ datab.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import MySQLdb as mdb
+import sys
+
+con=mdb.connect(host="localhost",user="philippe",passwd="bitagenou",db="elevage")
+
+cur = con.cursor()
+cur.execute("SELECT VERSION()")
+
+ver = cur.fetchone()
+    
+print("Database version : %s " % ver)
+	  
+
+	 # SQL query string
+
+#    	sqlQuery = "describe questions_section"   
+
+    	# Execute the sqlQuery
+
+#    	cur.execute(sqlQuery)
